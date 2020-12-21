c707401525f38f0ce6709a2ddbcaed2275e6d176
bong_u <thsqhddn7@naver.com>
2020-12-22 00:43:20 +0900

    검색해서 10개 가져옴 -> 너무 느림

--- instagram/installoader.py
+++ instagram/installoader.py
@@ -3,16 +3,18 @@ from instaloader import Instaloader
 loader = Instaloader()
 
 NUM_POSTS = 10
-count = 0
+
 
 def search_posts(query):
     posts = loader.get_hashtag_posts(query)
+    count = 0
 
 
     for post in posts:
         print(post.profile)
         print(post.date)
 
+        count += 1
         if count == NUM_POSTS:
             break
 
