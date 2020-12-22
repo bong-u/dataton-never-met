dcb680f91f897b1572a28467829708bd9d56a213
bong_u <thsqhddn7@naver.com>
2020-12-22 17:35:12 +0900

    selenium 성공 100개 끌어오기, 근데 너무느리다

--- instagram/MyInstaCrawler.py
+++ instagram/MyInstaCrawler.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
-from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from time import sleep, time
 from bs4 import BeautifulSoup
 
 
@@ -9,11 +12,17 @@ pwd = input('passwd?')
 BASE_URL = 'https://www.instagram.com/'
 SEARCH_URL = BASE_URL + 'explore/tags/'
 
-options = webdriver.ChromeOptions() 
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu") 
+options.add_argument("disable-infobars")
+options.add_argument("--disable-extensions")
+options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 driver = webdriver.Chrome('chromedriver.exe', options=options)
 driver.get (BASE_URL)
-sleep(1)
+sleep(3)
 
 def login():
     input_id = driver.find_elements_by_css_selector('input._2hvTZ.pexuQ.zyHYP')[0]
@@ -27,28 +36,34 @@ def login():
 
 def search(query):
     driver.get(SEARCH_URL + query)
-    sleep(2)
+    sleep(2.5)
 
 def crawl():
 
     first = driver.find_element_by_css_selector('div._9AhH0')
     first.click()
     sleep(1)
+
+    with open('result.txt', 'w') as file:
     
-    for i in range(10):
-        soup = BeautifulSoup(driver.page_source, 'lxml')
+        for i in range(100):
+            soup = BeautifulSoup(driver.page_source, 'lxml')
 
-        date = soup.select('time._1o9PC.Nzb55')[0]['datetime'][:10]
 
-        print (date)
+            date = WebDriverWait(driver, 5).until(
+                EC.visibility_of_element_located(
+                    (By.CSS_SELECTOR, 'time._1o9PC.Nzb55'))).get_attribute("datetime")[:10]
+
+            print (date)
+            file.write(date + '\n')
         
-        right = driver.find_element_by_css_selector ('a.coreSpriteRightPaginationArrow')
-        right.click()
-        sleep(0.5)
-    
-    
+            right = driver.find_element_by_css_selector ('a.coreSpriteRightPaginationArrow')
+            right.click()
 
 
+start = time()
 login()
 search('치킨')
 crawl()
+
+print (time() - start)
