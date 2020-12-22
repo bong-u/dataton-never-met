c521df31861fe11f463cd235c8987a5814d6b56b
bong_u <thsqhddn7@naver.com>
2020-12-22 13:48:07 +0900

    로그인없이 crawl 성공

--- instagram/MyInstaCrawler.py
+++ instagram/MyInstaCrawler.py
@@ -1,24 +1,45 @@
 import requests
 import bs4
+import json
+
+'''
+def auth():
+    query = '치킨'
+    
+    session = requests.Session()
+    session.cookies.set('ig_pr', '1')
+    session.headers = {'Referer' : 'https://www.instagram.com/',
+                       'user-agent' : 'Instagram 123.0.0.21.114 (iPhone; CPU iPhone OS 11_4 like Mac OS X; en_US; en-US; scale=2.00; 750x1334) AppleWebKit/605.1.15' }
+
+    req = session.get('https://www.instagram.com/')
+
+    session.headers.update({'X-CSRFToken' : req.cookies['csrftoken']})
+    session.headers.update({'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'})
+
+    res = session.get('https://www.instagram.com/explore/tags/%s' % query)
+
+    print(res)
+    print(res.text)
+'''
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
 
 def search(query):
-    url = 'https://www.youtube.com/results'
-    header = {'User-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'}
-    payload = {'search_query' : '족발'}
+    url = 'https://www.instagram.com/graphql/query/?query_hash=f92f56d47dc7a55b606908374b43a314&variables= {"tag_name": "%s", "first": 10}' % query
+    header = {'User-agent' : USER_AGENT}
     
-    res = requests.get(url, headers=header, params=payload)
+    res = requests.get(url, headers = header)
 
     print(res)
     
-    f = open('res.txt', 'w', -1, 'utf-8')
-    f.write (res.text)
-    
+    #f = open('res.txt', 'w', -1, 'utf-8')
+    #.write (res.text)
+
+    #dict = json.loads(res.text)
 
     print (res.text)
     
     #res = bs4.BeautifulSoup(res.text, "lxml")
     
-
-search('족발')
+search ('족발')
 
 
