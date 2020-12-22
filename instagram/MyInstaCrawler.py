a04ea5de57820a3852e2020bdbe8b1a4965acc87
bong_u <thsqhddn7@naver.com>
2020-12-22 16:31:04 +0900

    10번 가져오기

--- instagram/MyInstaCrawler.py
+++ instagram/MyInstaCrawler.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from time import sleep
+from bs4 import BeautifulSoup
 
 
 email = input('email?')
@@ -12,7 +13,7 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 driver = webdriver.Chrome('chromedriver.exe', options=options)
 driver.get (BASE_URL)
-sleep(2)
+sleep(1)
 
 def login():
     input_id = driver.find_elements_by_css_selector('input._2hvTZ.pexuQ.zyHYP')[0]
@@ -22,12 +23,32 @@ def login():
     input_pw.clear()
     input_pw.send_keys(pwd)
     input_pw.submit()
-    sleep(5)
+    sleep(2)
 
-def search():
-    drvier.get(SEARCH_URL)
+def search(query):
+    driver.get(SEARCH_URL + query)
+    sleep(2)
 
+def crawl():
+
+    first = driver.find_element_by_css_selector('div._9AhH0')
+    first.click()
+    sleep(1)
+    
+    for i in range(10):
+        soup = BeautifulSoup(driver.page_source, 'lxml')
+
+        date = soup.select('time._1o9PC.Nzb55')[0]['datetime'][:10]
+
+        print (date)
+        
+        right = driver.find_element_by_css_selector ('a.coreSpriteRightPaginationArrow')
+        right.click()
+        sleep(0.5)
+    
+    
 
 
 login()
 search('치킨')
+crawl()
